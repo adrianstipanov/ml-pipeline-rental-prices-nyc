0f4eea73d9df710b803d1288518a3cb1fdfadc6b
Adrian Stipanov <adrian.stipanov@gmail.com>
2022-04-21 17:08:07 +0200

    fix lat and long coordinates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Restrict long and lat
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Save the results to a CSV file
     logger.info("Save the results to CSV")
     df.to_csv(args.output_artifact, index=False)
